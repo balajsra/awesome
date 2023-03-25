dd150ca729059c6e34b44174ce410f2d4c2418c7
Sravan Balaji <balajsra@umich.edu>
2023-03-25 06:59:59 -0400

    Awesome WM Micky Submodule
    - Add awesomewm-micky as submodule
    - Automatically centers mouse cursor on the focused window

--- rc.lua
+++ rc.lua
@@ -17,6 +17,9 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
+-- Automatically move and center mouse cursors to the focused window
+require("awesomewm-micky")
+
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
