28244bbee6f3d4f2e455e8e5be3047dc7c0738a1
Sravan Balaji <balajsra@umich.edu>
2023-03-26 10:04:47 -0400

    Restart Keybinding
    - Change to match keybinding that
      session control script calls

--- rc.lua
+++ rc.lua
@@ -267,7 +267,7 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey, "Shift"   }, "Return", function () awful.spawn(terminal_cmd) end,
               {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r", awesome.restart,
+    awful.key({ modkey, "Shift", "Control" }, "q", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
